ebd2ae61939322c985ba3b8794e340d4b82e1c24
Nathan Souphanthong <56615828+NathanSouphanthong@users.noreply.github.com>
2023-07-17 09:02:35 -0400

    Add BT event handler and sequencing

--- BluetoothSerial/BluetoothSerial.ino
+++ BluetoothSerial/BluetoothSerial.ino
@@ -15,17 +15,45 @@ AccelStepper betaStepper(HALFSTEP,19, 22, 23, 18);
 // Creates an instance of BluetoothSerial called SerialBT
 BluetoothSerial SerialBT;
 
+// Motor Variables 
+double azimuthSteps = NULL; 
+double betaSteps = NULL; 
+double incrementAngle = NULL;
+double incrementTime = NULL; 
+
+
+bool cong = false; 
+
+void BT_EventHandler(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+  if (event == ESP_SPP_START_EVT) {
+    Serial.println("Initialized SPP");
+  }
+  else if (event == ESP_SPP_SRV_OPEN_EVT ) {
+    Serial.println("Client connected");
+  }
+  else if (event == ESP_SPP_CLOSE_EVT  ) {
+    Serial.println("Client disconnected");
+  }
+  else if (event == ESP_SPP_CONG_EVT){
+    cong = true;
+    Serial.println("Client not listening");
+  }
+}
+
 void setup() {
   Serial.begin(115200);         // Initialize a serial communication at a baud rate of 115200
   SerialBT.begin("SYDEGroup16");   // Bluetooth device name SYDEGroup16
   Serial.println("The device started, now you can pair it with bluetooth!");
 
-  //Setup defaults for both steppers 
+  // Setup defaults for both steppers 
   betaStepper.setMaxSpeed(100);
   betaStepper.setAcceleration(20);
 
   azimuthStepper.setMaxSpeed(100);
   azimuthStepper.setAcceleration(20);
+
+   // Register the callbacks defined above (most important: congestion)
+  SerialBT.register_callback(BT_EventHandler);
 }
 
 /* 
@@ -40,7 +68,13 @@ void setup() {
 */
 
 void loop() {
-  if (SerialBT.available()) {
+  // If our variables aren't populated, continue to read from the Bluetooth Serial connection
+  if (SerialBT.available() && 
+      (azimuthSteps == NULL || 
+      betaSteps == NULL ||
+      incrementAngle == NULL ||
+      incrementTime == NULL)
+    ){
     String input = SerialBT.readString();
     //Process the input string 
     String id = input.substring(0,1);
@@ -48,28 +82,48 @@ void loop() {
     value.trim();
 
     if(id == String('A')){
-      double steps = value.toInt() * 5.69 * 2;
-      azimuthStepper.moveTo(steps);
-
-      Serial.println(value.toInt()); 
-      Serial.println();
+      azimuthSteps = value.toInt() * 5.69 * 2;
+      azimuthStepper.moveTo(azimuthSteps);
+      Serial.print("Azimuth angle set to: ");
+      Serial.print(value.toInt()); 
+      Serial.println(" degrees");
     } else if (id == String('B')){ 
-      double steps = value.toInt() * 5.69 * 2;
-      betaStepper.moveTo(steps);
-
-      Serial.println(value.toInt()); 
-      Serial.println();
+      if(value.toInt() <= 90){ //Our Beta Stepper should not exceed 90 degrees or go below 0
+        betaSteps = value.toInt() * 5.69 * 2;
+        betaStepper.moveTo(betaSteps);
+        Serial.print("Beta angle set to: ");
+        Serial.print(value.toInt()); 
+        Serial.println(" degrees");
+      }
     } else if (id == String('I')) {
-      int adjustmentAngle = value.substring(0,value.indexOf(',')).toInt();
-      int adjustmentTimeMins = value.substring(value.indexOf(',') + 1).toInt();
-
-      Serial.println(adjustmentAngle); 
-      Serial.println(adjustmentTimeMins); 
-      Serial.println();
+      incrementAngle = value.substring(0,value.indexOf(',')).toInt();
+      incrementTime = value.substring(value.indexOf(',') + 1).toInt();
+      Serial.print("Increment angle set to: ");
+      Serial.print(incrementAngle); 
+      Serial.println(" degrees");
+      Serial.print("Increment time set to: ");
+      Serial.print(incrementTime); 
+      Serial.println(" minutes");
     }
   }
-  
-  // Set the speed and run the steppers (these should be called as often as possible)
+
+  // This is executed after we set the initial position
+  if (incrementAngle != NULL && incrementTime != NULL && azimuthStepper.distanceToGo() == 0){ 
+    Serial.print("Starting sequence, will move motor by ");
+    Serial.print(incrementAngle);
+    Serial.print(" degrees in ");
+    Serial.print(incrementTime);
+    Serial.println(" minutes");
+    long incrementTimeMS = incrementTime * 60000;
+    delay(incrementTimeMS);
+    Serial.println("Moving motor");
+    // Here we should check if the increment angle is within bounds 0 < x < 90 before incrementing
+    // Serial.println(azimuthStepper.currentPosition()/(5.69 * 2));
+    azimuthStepper.move(incrementAngle*5.69*2); //move relatively
+    azimuthStepper.setSpeed(100);
+    azimuthStepper.runSpeedToPosition();
+  }
+    // Set the speed and run the steppers (these should be called as often as possible)
   azimuthStepper.setSpeed(100);
   azimuthStepper.runSpeedToPosition();
   betaStepper.setSpeed(100);
